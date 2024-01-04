ee3e881440d85ecc4b4e3dbecb8bc73e6bef4516
Fernando Shibata <fsshibata.ntt@rd.com.br>
2024-01-04 09:02:31 -0300

    Variaveis

---
+++ 1 - Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,10 @@
+package auxiliar
+
+import (
+	"fmt"
+)
+
+func Escrever() { // A primeira letra da função define se é public ou private
+	fmt.Println("Escrevendo do pacote auxiliar")
+	escrever2()
+}
---
+++ 1 - Pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+// função iniciada com letra minúscula tem contexto package
+func escrever2() {
+	fmt.Println("Escrevendo pela função 2")
+}
