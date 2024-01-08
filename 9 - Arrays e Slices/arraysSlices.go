81d1a71493de9cccb0f24065cc7a6867c171b7de
Fernando Shin <b455.indignation@gmail.com>
2024-01-08 10:44:22 -0300

    Como slices funcionam

--- 9 - Arrays e Slices/arraysSlices.go
+++ 9 - Arrays e Slices/arraysSlices.go
@@ -32,4 +32,29 @@ func main() {
 
 	array2[1] = "Posição alterada"
 	fmt.Println(slice2)
+
+	// Arrays internos
+	fmt.Println("-------------------------------------")
+
+	slice3 := make([]float32, 10, 11)
+	// make cria um array de 15 posições e retorna um slice
+	// com as primeiras 10 posições
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // length
+	fmt.Println(cap(slice3)) // capacity
+
+	slice3 = append(slice3, 5)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // 11
+	fmt.Println(cap(slice3)) // 11
+
+	slice3 = append(slice3, 5)
+	fmt.Println(len(slice3)) // 12
+	fmt.Println(cap(slice3)) // 24. O go, quando vê que vai extourar o slice, ele dobra o seu tamanho
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
 }
