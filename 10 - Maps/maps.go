df68f3fe27d4068f92f66d249821d14586262154
Fernando Shin <b455.indignation@gmail.com>
2024-01-08 11:16:42 -0300

    Maps

---
+++ 10 - Maps/maps.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Maps")
+
+	usuario := map[string]string{
+		"nome":      "Pedro",
+		"sobrenome": "Silva",
+	}
+
+	fmt.Println(usuario)
+	fmt.Println(usuario["nome"])
+
+	usuario2 := map[string]map[string]string{
+		"nome": {
+			"primeiro": "João",
+			"segundo":  "Pedro",
+		},
+		"curso": {
+			"nome":   "Engenharia",
+			"campus": "Campus 1",
+		},
+	}
+
+	fmt.Println(usuario2)
+	delete(usuario2, "curso")
+	fmt.Println(usuario2)
+
+	usuario2["signo"] = map[string]string{
+		"nome": "Gêmeos",
+	}
+
+	fmt.Println(usuario2)
+}
