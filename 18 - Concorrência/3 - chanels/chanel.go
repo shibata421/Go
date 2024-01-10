8d26558462a773bf699628201e4850135862ac0f
Fernando Shin <b455.indignation@gmail.com>
2024-01-10 16:35:23 -0300

    Canais

---
+++ 18 - Concorrência/3 - chanels/chanel.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	canal := make(chan string)
+
+	go escrever("Olá Mundo", canal)
+
+	// for {
+	// 	mensagem, aberto := <-canal
+
+	// 	if !aberto {
+	// 		break
+	// 	}
+
+	// 	fmt.Println(mensagem)
+	// }
+
+	for mensagem := range canal {
+		fmt.Println(mensagem)
+	}
+}
+
+func escrever(texto string, canal chan string) {
+	for i := 0; i < 5; i++ {
+		canal <- texto
+		time.Sleep(time.Second)
+	}
+
+	close(canal)
+}
